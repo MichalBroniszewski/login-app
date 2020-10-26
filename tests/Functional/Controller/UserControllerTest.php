a9809181aa2be876a32f5893a462a628fa1618ca
michal.broniszewski <michal.broniszewski@picodi.com>
2020-10-26 20:43:05 +0100

    tests

--- tests/Functional/Controller/UserControllerTest.php
+++ tests/Functional/Controller/UserControllerTest.php
@@ -7,24 +7,54 @@
  */
 declare(strict_types=1);
 
-namespace Functional\Controller;
+namespace App\Tests\Functional\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use PHPUnit\Framework\IncompleteTestError;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testListActionWhenAllowed(): void
+    public function testListActionWhenNotLoggedIn(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/user/list');
+        self::assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testListAction(): void
     {
         $client = static::createClient();
 
-        $userRepository = static::$container->get(UserRepository::class);
-        $testUser = $userRepository->find(1);
-
+        $testUser = $this->getEnabledUser();
+        if (!$testUser) {
+            throw new IncompleteTestError(
+                'There is no user enabled for logged in. Please make sure to provide this data, before running the test again.'
+            );
+        }
         $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/user/list');
 
-        $client->request('GET', '/user/list');
         self::assertResponseIsSuccessful();
+        self::assertPageTitleContains('User list');
         self::assertSelectorTextContains('h3', 'List of application users');
+        self::assertLessThanOrEqual(10, $crawler->filter('div.user-container')->count());
+        self::assertEquals(1, $crawler->filter('div.navigation')->count());
+    }
+
+    private function getEnabledUser(): ?User
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = static::$container->get(UserRepository::class);
+        $qb = $userRepository->createQueryBuilder('u');
+        $result = $qb->select()
+            ->where($qb->expr()->eq('u.enabled', 1))
+            ->orderBy('u.id')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getResult();
+
+        return reset($result);
     }
 }
---
+++ tests/Unit/Service/UserManagerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * user: michal
+ * michal.broniszewski@picodi.com
+ * 26.10.2020
+ */
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Service;
+
+use App\Entity\User;
+use App\Service\UserManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserManagerTest extends TestCase
+{
+    private UserManager $testSubject;
+
+    /**
+     * @var MockObject|UserPasswordEncoderInterface
+     */
+    private MockObject $passwordEncoder;
+
+    /**
+     * @var MockObject|EntityManagerInterface
+     */
+    private MockObject $entityManager;
+
+    protected function setUp(): void
+    {
+        $this->passwordEncoder = $this->getMockBuilder(UserPasswordEncoderInterface::class)
+            ->getMock();
+        $this->entityManager = $this->getMockBuilder(EntityManagerInterface::class)
+            ->getMock();
+
+        $this->testSubject = new UserManager(
+            $this->passwordEncoder,
+            $this->entityManager
+        );
+    }
+
+    public function testSaveNewUser(): void
+    {
+        $user = $this->getUser();
+
+        $this->passwordEncoder->expects($this->once())
+            ->method('encodePassword');
+
+        $this->entityManager->expects($this->once())
+            ->method('persist')
+            ->with($user);
+
+        $this->entityManager->expects($this->once())
+            ->method('flush');
+
+        $this->testSubject->saveNewUser($user);
+    }
+
+    public function testDisableUser(): void
+    {
+        $user = $this->getUser();
+
+        $this->entityManager->expects($this->once())
+            ->method('flush');
+
+        $this->testSubject->saveNewUser($user);
+    }
+
+    private function getUser(): User
+    {
+        $user = new User();
+        $user->setUsername('username');
+        $user->setPlainPassword('secrect');
+
+        return $user;
+    }
+}
